a8d84bf753af6c97d47797bf75b4948e114938ad
Kiaryy <martin360.moron@gmail.com>
2022-11-27 16:46:04 -0300

    Main
    Este será el script principal, ahora mismo lo único que hace es importar el script del menú. Luego contiene un movimiento simple con las flechas

--- main.py
+++ main.py
@@ -1,28 +1,37 @@
-import pygame, sys
+import pygame,sys
 from pygame.locals import *
-
-pygame.init()
+import menu 
 width,height = 800,600
+clock = pygame.time.Clock()
+pygame.init()
 screen = pygame.display.set_mode((width, height))
 
+def sex():
+    bg = (125, 125, 125)
+    x = width/2
+    y = height/2
 
-
-
-def menu():
-    bgc = (0,0,0)
-    menu_font = pygame.font.SysFont('arialcursiva', 50)
-    game_name = menu_font.render("Juegardo", False, (255, 255, 255))
-    
     while True:
-        screen.fill(bgc)
-        screen.blit(game_name,(width/2-80, height/4))
-
+        screen.fill(bg)
+        keys = pygame.key.get_pressed()
+        pygame.draw.rect (screen, (166, 30, 30,), (x, y, 100, 50))
+        if keys[K_DOWN]:
+            y += 1
+        if keys[K_UP]:
+            y -= 1
+        if keys[K_RIGHT]:
+            x += 1
+        if keys[K_LEFT]:
+            x -= 1
+        
+        clock.tick(60)
         pygame.display.update()
-        pygame.time.delay(10)
         for event in pygame.event.get():
             if event.type==QUIT:
                 pygame.quit()
                 sys.exit()
 
+menu.menu()
+sex()
 
-menu()
+
