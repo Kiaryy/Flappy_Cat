6f22556a9fa8a3b0d43e2153e0b4301d9f937c64
Kiaryy <martin360.moron@gmail.com>
2022-12-01 16:29:26 -0300

    Player Class
    Contains variables

---
+++ player.py
@@ -0,0 +1,10 @@
+import pygame
+from utils import resource_path
+class player_obj:
+    pos = pygame.Vector2()
+    pos.xy = 350,300
+    vel = pygame.Vector2()
+    sprite = pygame.image.load(resource_path('assets\\player.png'))
+    vel.xy = 3,0
+    angle = 0
+    jumpforce = 4.5
